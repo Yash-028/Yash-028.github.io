fcf85aa88834d598e74e3b300a5adb85b2657698
Yash Chauhan <ychauhan2@horizon.csueastbay.edu>
2023-05-09 13:40:37 -0700

    Delete backend directory

--- backend/models/favSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-
-const userSchema=new mongoose.Schema({},
-{strict:false}
-)
-	
-const AppUserFav=mongoose.model("AppUserFav",userSchema)
-module.exports=AppUserFav;
